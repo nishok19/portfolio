4ffc8b438a9a7c51e3af4479c1282fb29a168a90
nishok19 <58786882+nishok19@users.noreply.github.com>
2023-09-17 01:46:07 +0530

    Data updation

--- DataJson/htmlProjects.js
+++ DataJson/htmlProjects.js
@@ -2,7 +2,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 1",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     // img: require("../images/todo-sample.png"),
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-1/main/output1.png",
     srclink: "https://github.com/nishok19/html-css-proj-1",
@@ -12,7 +12,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 2",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-2/main/output2.png",
     srclink: "https://github.com/nishok19/html-css-proj-2",
     livelink: "https://nishok-html-css-proj2.netlify.app/",
@@ -21,7 +21,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 3",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-3/main/output3.png",
     srclink: "https://github.com/nishok19/html-css-proj-3",
     livelink: "https://nishok-html-css-proj3.netlify.app/",
@@ -30,7 +30,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 4",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     // img: require("../images/todo-sample.png"),
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-4/main/output4.png",
     srclink: "https://github.com/nishok19/html-css-proj-4",
@@ -40,7 +40,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 5",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-5/main/output5.png",
     srclink: "https://github.com/nishok19/html-css-proj-5",
     livelink: "https://nishok-html-css-proj5.netlify.app/",
@@ -49,7 +49,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 6",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-6/main/output6.png",
     srclink: "https://github.com/nishok19/html-css-proj-6",
     livelink: "https://nishok-html-css-proj6.netlify.app/",
@@ -58,7 +58,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 7",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     // img: require("../images/todo-sample.png"),
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-7/main/output7.png",
     srclink: "https://github.com/nishok19/html-css-proj-7",
@@ -68,7 +68,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 8",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-8/main/output8.png",
     srclink: "https://github.com/nishok19/html-css-proj-8",
     livelink: "https://nishok-html-css-proj8.netlify.app/",
@@ -77,7 +77,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 9",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-9/main/output9.png",
     srclink: "https://github.com/nishok19/html-css-proj-9",
     livelink: "https://nishok-html-css-proj9.netlify.app/",
@@ -86,7 +86,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 10",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     // img: require("../images/todo-sample.png"),
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-10/main/output10.png",
     srclink: "https://github.com/nishok19/html-css-proj-10",
@@ -96,7 +96,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 11",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-11/main/output11.png",
     srclink: "https://github.com/nishok19/html-css-proj-11",
     livelink: "https://nishok-html-css-proj11.netlify.app/",
@@ -105,7 +105,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 12",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-12/main/output12.png",
     srclink: "https://github.com/nishok19/html-css-proj-12",
     livelink: "https://nishok-html-css-proj12.netlify.app/",
@@ -114,7 +114,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 13",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     // img: require("../images/todo-sample.png"),
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-13/main/output13.png",
     srclink: "https://github.com/nishok19/html-css-proj-13",
@@ -124,7 +124,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 14",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-14/main/output14.png",
     srclink: "https://github.com/nishok19/html-css-proj-14",
     livelink: "https://nishok-html-css-proj14.netlify.app/",
@@ -133,7 +133,7 @@ const htmlprojects = [
   {
     name: "HTML&CSS Project - 15",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat tellus condimentum nisl adipiscing. Facilisis arcu pellentesque proin lobortis pharetra, nunc, purus.",
+      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
     img: "https://raw.githubusercontent.com/nishok19/html-css-proj-15/main/output15.png",
     srclink: "https://github.com/nishok19/html-css-proj-15",
     livelink: "https://nishok-html-css-proj15.netlify.app/",
