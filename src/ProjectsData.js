85bd4fadc41a5bee6cf5214618bb1d655f2f3ce2
nishok19 <58786882+nishok19@users.noreply.github.com>
2025-01-24 23:24:20 +0530

    Update ProjectsData.js

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -1,5 +1,15 @@
 export const projects = [
+  
   {
+    name: "Image E-Commerce Web Application",
+    description:
+      "I'm excited to introduce a dynamic, feature-rich E-Commerce application. It's a full-stack web project using Next.js, Express, MongoDB, and Redux, showcasing my web development skills.",
+    img: "https://github.com/nishok19/portfolio/blob/main/public/images/ImgEcom-1.png?raw=true",
+    srclink: "https://github.com/nishok19/portfolio",
+    livelink: "https://nishok.my",
+    type: "react",
+  },
+ {
     name: "E-Commerce Web Application",
     description:
       "I'm excited to introduce a dynamic, feature-rich E-Commerce application. It's a full-stack web project using Next.js, Express, MongoDB, and Redux, showcasing my web development skills.",
@@ -26,147 +36,7 @@ export const projects = [
     livelink: "https://github.com/nishok19/react-redux-auth-protected",
     type: "backend",
   },
-  {
-    name: "HTML&CSS Project - 1",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    // img: require("../images/todo-sample.png"),
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-1/main/output1.png",
-    srclink: "https://github.com/nishok19/html-css-proj-1",
-    livelink: "https://nishok-html-css-proj1.netlify.app/",
-    type: "react",
-  },
-  {
-    name: "HTML&CSS Project - 2",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-2/main/output2.png",
-    srclink: "https://github.com/nishok19/html-css-proj-2",
-    livelink: "https://nishok-html-css-proj2.netlify.app/",
-    type: "react",
-  },
-  {
-    name: "HTML&CSS Project - 3",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-3/main/output3.png",
-    srclink: "https://github.com/nishok19/html-css-proj-3",
-    livelink: "https://nishok-html-css-proj3.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 4",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    // img: require("../images/todo-sample.png"),
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-4/main/output4.png",
-    srclink: "https://github.com/nishok19/html-css-proj-4",
-    livelink: "https://nishok-html-css-proj4.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 5",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-5/main/output5.png",
-    srclink: "https://github.com/nishok19/html-css-proj-5",
-    livelink: "https://nishok-html-css-proj5.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 6",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-6/main/output6.png",
-    srclink: "https://github.com/nishok19/html-css-proj-6",
-    livelink: "https://nishok-html-css-proj6.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 7",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    // img: require("../images/todo-sample.png"),
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-7/main/output7.png",
-    srclink: "https://github.com/nishok19/html-css-proj-7",
-    livelink: "https://nishok-html-css-proj7.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 8",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-8/main/output8.png",
-    srclink: "https://github.com/nishok19/html-css-proj-8",
-    livelink: "https://nishok-html-css-proj8.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 9",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-9/main/output9.png",
-    srclink: "https://github.com/nishok19/html-css-proj-9",
-    livelink: "https://nishok-html-css-proj9.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 10",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    // img: require("../images/todo-sample.png"),
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-10/main/output10.png",
-    srclink: "https://github.com/nishok19/html-css-proj-10",
-    livelink: "https://nishok-html-css-proj10.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 11",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-11/main/output11.png",
-    srclink: "https://github.com/nishok19/html-css-proj-11",
-    livelink: "https://nishok-html-css-proj11.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 12",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-12/main/output12.png",
-    srclink: "https://github.com/nishok19/html-css-proj-12",
-    livelink: "https://nishok-html-css-proj12.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 13",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    // img: require("../images/todo-sample.png"),
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-13/main/output13.png",
-    srclink: "https://github.com/nishok19/html-css-proj-13",
-    livelink: "https://nishok-html-css-proj13.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 14",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-14/main/output14.png",
-    srclink: "https://github.com/nishok19/html-css-proj-14",
-    livelink: "https://nishok-html-css-proj14.netlify.app/",
-    type: "htmlcss",
-  },
-  {
-    name: "HTML&CSS Project - 15",
-    description:
-      "My projects are a testament to my proficiency in HTML, CSS, JavaScript, and Tailwind CSS, combining these technologies to create visually appealing and highly functional websites.",
-    img: "https://raw.githubusercontent.com/nishok19/html-css-proj-15/main/output15.png",
-    srclink: "https://github.com/nishok19/html-css-proj-15",
-    livelink: "https://nishok-html-css-proj15.netlify.app/",
-    type: "htmlcss",
-  },
-];
+ ];
 
 export const skills = [
   {
